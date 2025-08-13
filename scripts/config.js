a725893fb1ab013cdb443df5545973a0970ba514
IoriImasu <127693054+saaedimam@users.noreply.github.com>
2025-08-14 01:03:49 +0600

    chore: remove build and package files

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+window.CONFIG = {
+  "NEWS_API_URL": "https://jsonplaceholder.typicode.com/posts?_limit=3",
+  "CONTACT_ENDPOINT": "https://httpbin.org/post",
+  "CONTACT_EMAIL": "info@ktlbd.com",
+  "RFQ_ENDPOINT": "https://httpbin.org/post",
+  "RFQ_EMAIL": "commercial@ktlbd.com"
+};
